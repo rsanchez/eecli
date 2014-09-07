9796fddd86cb3a407875d958b580bf1e463aeff0
Khaliq Gant <khaliqgant@gmail.com>
2014-09-07 18:32:26 -0400

    fix spelling error

--- src/Command/CreateChannelCommand.php
+++ src/Command/CreateChannelCommand.php
@@ -12,7 +12,7 @@ class CreateChannelCommand extends Command
     /**
      * {@inheritdoc}
      */
-    protected $name = 'craete:ee:channel';
+    protected $name = 'create:ee:channel';
 
     /**
      * {@inheritdoc}
