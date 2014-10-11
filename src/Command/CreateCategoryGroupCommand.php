a826e7e07a2515abffa2fb2f4f9485de59553537
Rob Sanchez <rob@robsanchez.com>
2014-10-11 10:28:20 -0500

    Add create:category_group command

---
+++ src/Command/CreateCategoryGroupCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace eecli\Command;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class CreateCategoryGroupCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $name = 'create:category_group';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $description = 'Create a category group.';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getArguments()
+    {
+        return array(
+            array(
+                'name', // name
+                InputArgument::REQUIRED, // mode
+                'The name of the category group.', // description
+            ),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function fire()
+    {
+        $this->getApplication()->newInstance('\\eecli\\CodeIgniter\\Controller\\AdminContentController');
+
+        $name = $this->argument('name');
+
+        $_POST = array(
+            'group_name' => $name,
+            'field_html_formatting' => 'all',
+            'exclude_group' => '0',
+        );
+
+        ee()->update_category_group();
+
+        if (ee()->output->getErrorMessage()) {
+            $this->error(ee()->output->getErrorMessage());
+
+            return;
+        }
+
+        $query = ee()->db->select('group_id')
+            ->where('group_name', $name)
+            ->where('site_id', ee()->config->item('site_id'))
+            ->get('category_groups');
+
+        $this->info(sprintf('Category group %s (%s) created.', $name, $query->row('group_id')));
+
+        $query->free_result();
+    }
+}
