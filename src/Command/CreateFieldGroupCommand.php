381a46d163cfc666b54b92a9fcf46bf1935d736c
Rob Sanchez <rob@robsanchez.com>
2014-10-11 10:38:32 -0500

    Add create:field_group command

---
+++ src/Command/CreateFieldGroupCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace eecli\Command;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+
+class CreateFieldGroupCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $name = 'create:field_group';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $description = 'Create a field group.';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getArguments()
+    {
+        return array(
+            array(
+                'name', // name
+                InputArgument::REQUIRED, // mode
+                'The name of the field group.', // description
+            ),
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function fire()
+    {
+        $this->getApplication()->newInstance('\\eecli\\CodeIgniter\\Controller\\AdminContentController');
+
+        $name = $this->argument('name');
+
+        $_POST = array(
+            'group_name' => $name,
+        );
+
+        ee()->field_group_update();
+
+        if (ee()->output->getErrorMessage()) {
+            $this->error(ee()->output->getErrorMessage());
+
+            return;
+        }
+
+        $query = ee()->db->select('group_id')
+            ->where('group_name', $name)
+            ->where('site_id', ee()->config->item('site_id'))
+            ->get('field_groups');
+
+        $this->info(sprintf('Field group %s (%s) created.', $name, $query->row('group_id')));
+
+        $query->free_result();
+    }
+}
