930195f18366e0e2b6c9cf9cadc6453f50f5ce9b
Rob Sanchez <rob@robsanchez.com>
2015-03-07 11:02:11 -0600

    Allow group names in field_group, status_group and cat_group options in create:channel command
    Fixes #10

--- src/Command/CreateChannelCommand.php
+++ src/Command/CreateChannelCommand.php
@@ -50,21 +50,21 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
                 'field_group',
                 'f',
                 InputOption::VALUE_REQUIRED,
-                'Which field group do you want to assign this channel to?',
+                'Which field group (ID or name) do you want to assign this channel to?',
                 '',
             ),
             array(
                 'status_group',
                 's',
                 InputOption::VALUE_REQUIRED,
-                'Which status group do you want to assign this channel to?',
+                'Which status group (ID or name) do you want to assign this channel to?',
                 '',
             ),
             array(
                 'cat_group',
                 'c',
                 InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
-                'Which cat group(s) do you want to assign this channel to? Separate multiple with comma.',
+                'Which category group(s) (ID or name) do you want to assign this channel to? Separate multiple with comma.',
             ),
             array(
                 'channel_url',
@@ -102,7 +102,7 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
                 'deft_category',
                 null,
                 InputOption::VALUE_REQUIRED,
-                'What is the default category ID for this channel?',
+                'What is the default category (ID or name) for this channel?',
             ),
             array(
                 'new_field_group',
@@ -126,9 +126,21 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
 
         $fieldGroup = (string) $this->option('field_group');
 
-        $statusGroup = (string) $this->option('status_group');
+        if ($fieldGroup) {
+            // find the group ID by name
+            if (! is_numeric($fieldGroup)) {
+                $query = ee()->db->select('group_id')
+                    ->where('group_name', $fieldGroup)
+                    ->limit(1)
+                    ->get('field_groups');
 
-        if (! $fieldGroup) {
+                if ($query->num_rows() > 0) {
+                    $fieldGroup = $query->row('group_id');
+                }
+
+                $query->free_result();
+            }
+        } else {
             if ($this->option('new_field_group')) {
                 ee()->load->model('field_model');
 
@@ -155,7 +167,23 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
             }
         }
 
-        if (! $statusGroup) {
+        $statusGroup = (string) $this->option('status_group');
+
+        if ($statusGroup) {
+            // find the group ID by name
+            if (! is_numeric($statusGroup)) {
+                $query = ee()->db->select('group_id')
+                    ->where('group_name', $statusGroup)
+                    ->limit(1)
+                    ->get('status_groups');
+
+                if ($query->num_rows() > 0) {
+                    $statusGroup = $query->row('group_id');
+                }
+
+                $query->free_result();
+            }
+        } else {
             // trying to find the open/closed status group
             $query = ee()->db->select('group_id')
                 ->where('(SELECT COUNT(*) FROM exp_statuses WHERE exp_statuses.group_id = exp_status_groups.group_id) = 2', null, false)
@@ -171,11 +199,31 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
             $query->free_result();
         }
 
+        $catGroups = array();
+
+        foreach ($this->option('cat_group') as $catGroup) {
+            // find the group ID by name
+            if (! is_numeric($catGroup)) {
+                $query = ee()->db->select('group_id')
+                    ->where('group_name', $catGroup)
+                    ->limit(1)
+                    ->get('category_groups');
+
+                if ($query->num_rows() > 0) {
+                    $catGroup = $query->row('group_id');
+                }
+
+                $query->free_result();
+            }
+
+            $catGroups[] = $catGroup;
+        }
+
         $_POST = array(
             'channel_title' => $title,
             'channel_name' => $name,
             'duplicate_channel_prefs' => '',
-            'cat_group' => $this->option('cat_group'),
+            'cat_group' => $catGroups,
             'status_group' => $statusGroup,
             'field_group' => $fieldGroup,
             'channel_prefs_submit' => 'Submit',
@@ -193,13 +241,30 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
 
         $query->free_result();
 
+        $deftCategory = $this->option('deft_category');
+
+        // find the group ID by name
+        if (! is_numeric($deftCategory)) {
+            $query = ee()->db->select('cat_id')
+                ->where('cat_url_title', $deftCategory)
+                ->or_where('cat_name', $deftCategory)
+                ->limit(1)
+                ->get('categories');
+
+            if ($query->num_rows() > 0) {
+                $deftCategory = $query->row('cat_id');
+            }
+
+            $query->free_result();
+        }
+
         ee()->db->where('channel_id', $channelId)->update('channels', array(
             'channel_url' => $this->option('channel_url'),
             'channel_description' => $this->option('channel_description'),
             'default_entry_title' => $this->option('default_entry_title'),
             'url_title_prefix' => $this->option('url_title_prefix'),
             'deft_status' => $this->option('deft_status'),
-            'deft_category' => $this->option('deft_category'),
+            'deft_category' => $deftCategory,
         ));
 
         $this->info(sprintf('Channel %s (%s) created.', $name, $channelId));
@@ -216,8 +281,10 @@ class CreateChannelCommand extends Command implements HasExamples, HasOptionExam
             'Create a channel with the short name test_channel' => 'test_channel',
             'Create a channel with the title Test Channel' => 'test_channel "Test Channel"',
             'Create a channel with field group 5' => '--field_group=5 test_channel',
-            'Create a channel with status group 5' => '--status_group=5 test_channel',
-            'Create a channel with cat group 5 and 6' => '--cat_group="5,6" test_channel',
+            'Create a channel with field group "Blog"' => '--field_group="Blog" test_channel',
+            'Create a channel with status group "Statuses"' => '--status_group="Statuses" test_channel',
+            'Create a channel with category group 5 and 6' => '--cat_group="5,6" test_channel',
+            'Create a channel with category group "Apparel" and "Accessories"' => '--cat_group="Apparel,Accessories" test_channel',
             'Create a channel with new field group with same title as channel' => '--new_field_group test_channel',
         );
     }
