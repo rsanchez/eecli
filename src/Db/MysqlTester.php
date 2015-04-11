161e5ee6db947379bf540720ab09a79a39d20874
Rob Sanchez <rob@robsanchez.com>
2015-04-11 09:53:38 -0500

    Better DB testing error messages when mysql socket is bad

--- src/Db/MysqlTester.php
+++ src/Db/MysqlTester.php
@@ -9,23 +9,29 @@ class MysqlTester extends ConnectionTester
      */
     public function test()
     {
-        $connection = mysql_connect($this->hostname, $this->username, $this->password);
+        $connection = @mysql_connect($this->hostname, $this->username, $this->password);
 
         if ($connection === false) {
-            $this->setError(mysql_error());
+            $error = @mysql_error();
+
+            if ($error === 'No such file or directory') {
+                $error = sprintf('Could not connect to socket %s', @ini_get('mysql.default_socket'));
+            }
+
+            $this->setError($error);
 
             return false;
         }
 
-        $db = mysql_select_db($this->database, $connection);
+        $db = @mysql_select_db($this->database, $connection);
 
         if ($db === false) {
-            $this->setError(mysql_error());
+            $this->setError(@mysql_error());
 
             return false;
         }
 
-        mysql_close($connection);
+        @mysql_close($connection);
 
         return true;
     }
